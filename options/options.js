dec9f1e75e851002aa50966a2583436b3a160d71
Vadym Kolpak <vadym@tuluko.eu>
2024-08-21 00:23:20 +0300

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,10 +1,11 @@
 export const globalOptions = {
-  name: "tlkoa-49114",
-  url: "https://tlkoa-49114.botics.co",
-  api: "https://tlkoa-49114.botics.co/api/v1",
-  screenOptions: {
-    headerShown: false
-  }
+  "name": "tlkoa-49114",
+  "url": "https://tlkoa-49114.botics.co",
+  "api": "https://tlkoa-49114.botics.co/api/v1",
+  "screenOptions": {
+    "headerShown": false
+  },
+  "initialRoute": "TextRequestPage"
 };
 export const modulesOptions = {
   "@modules/app-menu": {
